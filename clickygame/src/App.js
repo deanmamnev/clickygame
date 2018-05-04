fd7d03618f296166def03a87c2c30fabc5ffa842
Dean Mamnev <deanmamnev@gmail.com>
2018-05-04 13:06:34 -0500

    once again

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,19 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import friends from "./friends.json";
 import './App.css';
 
+// <div className="App">
+// <header className="App-header">
+//   {/* <img src={logo} className="App-logo" alt="logo" /> */}
+//   <h1 className="App-title">Clicky Game</h1>
+// </header>
+// <p className="App-intro">
+//   Tell Dean Mamnev to finish this thing.
+// </p>
+// </div>
+
 class App extends Component {
+state = {
+  friends
+};
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Wrapper>
+  <div className="App">
+<header className="App-header">
+  {/* <img src={logo} className="App-logo" alt="logo" /> */}
+  <h1 className="App-title">Clicky Game</h1>
+</header>
+<p className="App-intro">
+  Tell Dean Mamnev to finish this thing.
+</p>
+</div>
+      <Title>
+        Welcome to the nightmare I call a project
+        </Title>
+        {this.state.friends.map(friend => (
+          <FriendCard
+            removeFriend={this.removeFriend}
+            id={friend.id}
+            key={friend.id}
+            name={friend.name}
+            image={friend.image}
+            occupation={friend.occupation}
+            location={friend.location}
+          />
+        ))}
+      </Wrapper>
     );
   }
 }
