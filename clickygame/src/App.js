56e1f2acfc6d11cc03e4a3ef137cdb677ca4948d
Dean Mamnev <deanmamnev@gmail.com>
2018-05-05 00:19:14 -0500

    AAAAAAAA-

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
-import FriendCard from "./components/FriendCard";
+// import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
-import friends from "./friends.json";
+import Jumbotron from './components/Jumbotron';
+// import friends from "./friends.json";
 import './App.css';
 
 // <div className="App">
@@ -16,41 +17,16 @@ import './App.css';
 // </p>
 // </div>
 
-class App extends Component {
-state = {
-  friends
-};
-
-
-  render() {
-    return (
-      <Wrapper>
-  <div className="App">
-<header className="App-header">
-  {/* <img src={logo} className="App-logo" alt="logo" /> */}
-  <h1 className="App-title">Clicky Game</h1>
-</header>
-<p className="App-intro">
-  Tell Dean Mamnev to finish this thing.
-</p>
-</div>
-      <Title>
-        Welcome to the nightmare I call a project
-        </Title>
-        {this.state.friends.map(friend => (
-          <FriendCard
-            removeFriend={this.removeFriend}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
-          />
-        ))}
-      </Wrapper>
-    );
-  }
-}
+const App = () => (
+  <div className="container-fluid mainContainer">
+    <Title>
+      Welcome to My Nightmare[change name]
+      </Title>
+      <Wrapper />
+      <Jumbotron />
+    
+    {/* <MainGameContainer />  */}
+  </div>
+);
 
 export default App;
